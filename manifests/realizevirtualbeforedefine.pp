64f9cc36b80cea3f5d46aff1d7a48e4cb00781ce
Ravi Kumar Nayak <ravinayak19@gmail.com>
2022-02-20 13:50:13 +0530

    Changes

--- manifests/realizevirtualbeforedefine.pp
+++ manifests/realizevirtualbeforedefine.pp
@@ -12,15 +12,17 @@ class virtualres::realizevirtualbeforedefine{
     tag    => 'defaultfiles, searchablefiles'
   }
 
-  # Virtual resources are ideally defined in 1 module and used in another through mostly include function
+  # Virtual resources are ideally defined in 1 class in and used in another class in same module. If we define all the virtual resources 
+  # as a top level module, and include it in another module, this will lead to code away from data which is an anti pattern
   # Including virtualresource module so we can see different search scenarios of resource collectors. If we realize a virtual file resource 
-  # before it is defined in the same file, then it actually creates that resource. There is no compilation error. One possible explanation 
-  # could be that evluation order of virtual resources does not impact its realizability in the same file, but if you declare virtual 
+  # before it is defined in the same file, then it actually creates that resource. There is no compilation error. Evluation order of virtual 
+  # resources does not impact its realizability in the same file, but if you declare virtual 
   # resource in another file, and include it, then the order of realizing the resource and definition does matter
+  # To realize a virtual resource which has been declared in another class, we must realize post inclusion. If you realize a virtual resource
+  # which has not been included through another class yet, this will lead to compilation error
   #
-  # If a virtual resource is defined in 1 module before its realization in another module, following 2 uce cases are possible:
-  #   1. realize: If you use realize a resource before it is defined, it should lead to compilation failure, but it does not, it works 
-  #               perfectly
+  # If a virtual resource is defined in 1 module before its realization in another module, following 2 use cases are possible:
+  #   1. realize: Evaluation order independent virtual resources are realized even if we include them before they are declared
   #   2. Resource Collector: since this is based on search, if the resource is not defined before search is performed, nothing results
 
   # resource collector before and after files are declared
